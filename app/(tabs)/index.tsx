89aa71c882f00ab4a636e305ba677645ed1be77b
Dhayanithi~ <dhayanithipartheeban@gmail.com>
2026-01-03 20:08:25 +0530

    todo-text-textinput-usestate

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Text, TextInput, View } from 'react-native';
 
 export default function HomeScreen() {
+  const [text, setText] = useState('');
+
   return (
     <View>
       <Text>Todo App</Text>
+      <TextInput
+        style={{
+          height: 40,
+          width: 200,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        onChangeText={setText}
+        value={text}
+      />
     </View>
   );
 }
