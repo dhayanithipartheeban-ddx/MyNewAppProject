116360b8164594becb97b13a37d749aff37198dc
Dhayanithi~ <dhayanithipartheeban@gmail.com>
2026-01-03 19:47:24 +0530

    my todo first text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,9 @@
-import React from 'react';
-import { Image, ScrollView, Text, TextInput, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-const App = () => {
+export default function HomeScreen() {
   return (
-    <ScrollView>
-      <Text>Some text</Text>
-      <View>
-        <Text>Some more text</Text>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{width: 200, height: 200}}
-        />
-      </View>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
-        defaultValue="You can type in me"
-      />
-    </ScrollView>
+    <View>
+      <Text>Todo App</Text>
+    </View>
   );
-};
-
-export default App;
+}
