09092fe5762e9df4ac9ca57fab84d639c90ea6ae
Dhayanithi~ <dhayanithipartheeban@gmail.com>
2026-01-04 17:04:32 +0530

    flat list  array of todo list and delete function

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
-import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { FlatList, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function HomeScreen() {
   const [text, setText] = useState('');
+  const [texts, setTexts] = useState<string[]>([]);
   const addTask = () => {
     // Add task logic here
+    if (!text) return;
+    setTexts([...texts, text]);
+    setText('');
     alert(text);
+  };
+  const deleteTask = (index: number) => {
+  setTexts(texts.filter((_, i) => i !== index));
   };
 
   return (
@@ -24,6 +31,16 @@ export default function HomeScreen() {
       <TouchableOpacity onPress={addTask}>
         <Text>Add Text</Text>
       </TouchableOpacity>
+
+      <FlatList
+        data={texts}
+        //renderItem={({ item }) => <Text>{item}</Text>}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity onPress={() => deleteTask(index)}>
+            <Text>{item}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
