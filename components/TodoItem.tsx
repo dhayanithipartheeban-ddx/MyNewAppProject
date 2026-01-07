92ead60ee42bc2d8e563fde64171d3b296e80863
Dhayanithi~ <dhayanithipartheeban@gmail.com>
2026-01-07 15:28:05 +0530

    toggle updates mxx 2 lines

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -18,11 +18,16 @@ export default function TodoItem({
 }: TodoItemProps) {
 
   const [expanded, setExpanded] = useState(false);
+  const [isExpandable, setIsExpandable] = useState(false);
 
   return (
     <TouchableOpacity
       activeOpacity={0.8}
-      onPress={() => setExpanded(!expanded)}
+      onPress={() => {
+        if (isExpandable) {
+          setExpanded(!expanded);
+        }
+      }}
       style={PropsStyle.container}
     >
       <View style={PropsStyle.textWrapper}>
@@ -32,22 +37,31 @@ export default function TodoItem({
             completed && PropsStyle.completed
           ]}
           numberOfLines={expanded ? undefined : 2}
+          onTextLayout={(e) => {
+            if (e.nativeEvent.lines.length > 2) {
+              setIsExpandable(true);
+            }
+          }}
         >
           {text}
         </Text>
       </View>
 
-      <View style={PropsStyle.actions}>
-        <TouchableOpacity onPress={onToggle}>
-          <Text style={PropsStyle.actionText}>
-            {completed ? '↩️' : '✅'}
-          </Text>
-        </TouchableOpacity>
+      {/* SHOW ACTIONS ONLY WHEN NOT EXPANDED */}
+      {!expanded && (
+        <View style={PropsStyle.actions}>
+          <TouchableOpacity onPress={onToggle}>
+            <Text style={PropsStyle.actionText}>
+              {completed ? '↩️' : '✅'}
+            </Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity onPress={onDelete}>
-          <MaterialIcons name="delete" size={24} color="#de8787ff" />
-        </TouchableOpacity>
-      </View>
+          <TouchableOpacity onPress={onDelete}>
+            <MaterialIcons name="delete" style={PropsStyle.delete} />
+          </TouchableOpacity>
+        </View>
+      )}
     </TouchableOpacity>
   );
+
 }
